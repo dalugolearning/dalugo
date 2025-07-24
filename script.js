4d10498b1ff6103014be71212a2f7c9243c3548a
dalugolearn <dalugolearn@email.com>
2025-07-24 14:34:37 +0700

    update konten website

--- script.js
+++ script.js
@@ -75,24 +75,29 @@ const cardItem = document.querySelector('.card-item');
 const cardItem2 = document.querySelector('.card-item2');
 const cardItem3 = document.querySelector('.card-item3');
 const cardItems = [cardItem, cardItem2, cardItem3];
-const computedStyle1 = getComputedStyle(cardItem);
-const cardIndex1 = computedStyle1.zIndex
-const computedStyle2 = getComputedStyle(cardItem2)
-const cardIndex2 = computedStyle2.zIndex
-const computedStyle3 = getComputedStyle(cardItem3)
-const cardIndex3 = computedStyle3.zIndex
-// console.log(Number(computedStyle.zIndex) + 1);
-console.log(cardIndex1, cardIndex2, cardIndex3)
 
-function cardNext() {
+function cardNext(active = true) {
     cardItems.forEach(card => {
         let currentIndex = getComputedStyle(card).zIndex;
-        if (currentIndex == cardIndex1) {
-            card.style.zIndex = cardIndex3;
-        } else if (currentIndex == cardIndex2) {
-            card.style.zIndex = cardIndex1;
-        } else if (currentIndex == cardIndex3) {
-            card.style.zIndex = cardIndex2;
+        if (currentIndex == 10) {
+            card.style.zIndex =Number(currentIndex) - 2;
+        } else if (currentIndex == 9) {
+            card.style.zIndex = Number(currentIndex) + 1;
+        } else if (currentIndex == 8) {
+            card.style.zIndex = Number(currentIndex) + 1;
+        }
+    });
+}
+
+function cardPrev(active = true) {
+    cardItems.forEach(card => {
+        let currentIndex = getComputedStyle(card).zIndex;
+        if (currentIndex == 10) {
+            card.style.zIndex = Number(currentIndex) - 1;
+        } else if (currentIndex == 9) {
+            card.style.zIndex = Number(currentIndex) - 1;
+        } else if (currentIndex == 8) {
+            card.style.zIndex = Number(currentIndex) + 2;
         }
     });
 }
